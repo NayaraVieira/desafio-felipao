6c680d8074447e2099672595a6f503af482da066
NayaraVieira <nayv.meira@outlook.com>
2024-05-21 10:45:58 -0300

    meu commit com o projeto solicitado finalizado entregando cada nível de herói conforme o seu XP

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-console.log('inicio do desafio');
+let nomeHeroi = 'Batman';
+let xpHeroi = 10500;
+let nivel = ['Ferro', 'Bronze', 'Prata', 'Ouro', 'Platina', 'Ascendente', 'Imortal', 'Radiante'];
+let i = 0;
+
+while(i < nivel.length){
+
+    if (xpHeroi <= 1000) {
+        console.log('O seu herói está nível ' + nivel[i]) //0
+        break;
+    } 
+    i++
+    
+    if (xpHeroi > 1000 && xpHeroi <= 2000) {
+        console.log('O seu herói está no nível ' + nivel[i])
+        break;
+    }
+    i++
+    
+    if (xpHeroi > 2000 && xpHeroi <= 5000) {
+        console.log('O seu herói está no nível ' + nivel[i])
+        break;
+    }
+    i++
+    
+    if (xpHeroi > 5000 && xpHeroi <= 7000) {
+        console.log('O seu herói está no nível ' + nivel[i])
+        break;
+    } 
+    i++
+    
+    if (xpHeroi > 7000 && xpHeroi <= 8000) {
+        console.log('O seu herói está no nível ' + nivel[i])
+        break;
+    } 
+    i++
+    
+    if (xpHeroi > 8000 && xpHeroi <= 9000) {
+        console.log('O seu herói está no nível ' + nivel[i])
+        break;
+    } 
+    i++
+    
+    if (xpHeroi > 9000 && xpHeroi <= 10000) {
+    
+        console.log('O seu herói está no nível ' + nivel[i])
+        break;
+    } else {
+        i++
+        console.log('O seu herói está no nível ' + nivel[i])
+        break;
+    }
+
+}
+console.log('O herói de nome ' + nomeHeroi + ' está no nível ' + nivel[i]);
+
+
+
